cbabca587e4b1261000e30b1a2e2d46ce2ec172b
= <=>
2020-10-02 00:57:48 +0530

    added meaning to words

--- lib/random_words.dart
+++ lib/random_words.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 import 'package:flutter/rendering.dart';
+import 'package:wordpair/worddef.dart';
 
 class RandomWords extends StatefulWidget {
   @override
@@ -8,8 +9,8 @@ class RandomWords extends StatefulWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
-  final _randomWordPairs = <WordPair>[];
-  final _savedWordPairs = Set<WordPair>();
+  final _randomWordPairs = <String>[];
+  final _savedWordPairs = Set<String>();
 
   Widget _buildList() {
     return ListView.builder(
@@ -21,47 +22,56 @@ class RandomWordsState extends State<RandomWords> {
           final index = item ~/ 2;
 
           if (index >= _randomWordPairs.length) {
-            _randomWordPairs.addAll(generateWordPairs().take(10));
+            _randomWordPairs.addAll(nouns);
           }
 
           return _buildRow(_randomWordPairs[index]);
         });
   }
 
-  Widget _buildRow(WordPair pair) {
+  Widget _buildRow(String pair) {
     final alreadySaved = _savedWordPairs.contains(pair);
     return ListTile(
-      title: Text(pair.asPascalCase, style: TextStyle(fontSize: 17.0)),
-      trailing: Icon(alreadySaved ? Icons.favorite : Icons.favorite_border,
-          color: alreadySaved ? Colors.red : null),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _savedWordPairs.remove(pair);
-          } else {
-            _savedWordPairs.add(pair);
-          }
-        });
-      },
+      title: Text(pair, style: TextStyle(fontSize: 17.0)),
+      trailing: IconButton(
+          icon: Icon(alreadySaved ? Icons.favorite : Icons.favorite_border,
+              color: alreadySaved ? Colors.red : null),
+          onPressed: () {
+            setState(() {
+              if (alreadySaved) {
+                _savedWordPairs.remove(pair);
+              } else {
+                _savedWordPairs.add(pair);
+              }
+            });
+          }),
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => Definition(word: pair)),
+      ),
     );
   }
 
   void _SavedPage() {
     Navigator.of(context)
         .push(MaterialPageRoute(builder: (BuildContext context) {
-      final Iterable<ListTile> tile = _savedWordPairs.map((WordPair pair) {
+      final Iterable<ListTile> tile = _savedWordPairs.map((String pair) {
         return ListTile(
-          title: Text(pair.asPascalCase, style: TextStyle(fontSize: 16.0)),
-          trailing: IconButton(
-              icon: Icon(Icons.delete),
-              onPressed: () {
-                setState(() {
-                  _savedWordPairs.remove(pair);
-                  Navigator.of(context).pop();
-                  _SavedPage();
-                });
-              }),
-        );
+            title: Text(pair, style: TextStyle(fontSize: 16.0)),
+            trailing: IconButton(
+                icon: Icon(Icons.delete),
+                onPressed: () {
+                  setState(() {
+                    _savedWordPairs.remove(pair);
+                    Navigator.of(context).pop();
+                    _SavedPage();
+                  });
+                }),
+            onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => Definition(word: pair)),
+                ));
       });
 
       final List<Widget> divided =
